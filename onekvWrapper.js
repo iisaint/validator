ae788e850a34577fe6402fc37a3e1dbab87f05e7
Yao-Hsin Chen <iisaintz@gmail.com>
2020-12-05 22:18:14 +0800

    clean up

--- onekvWrapper.js
+++ onekvWrapper.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 const moment = require('moment');
 const ChainData = require('./chaindata');
-const chaindata = require('./chaindata');
+
+const KUSAMA_DECIMAL = 1000000000000;
 
 module.exports = class OnekvWrapper {
   constructor(handler) {
@@ -16,9 +17,7 @@ module.exports = class OnekvWrapper {
       
       // retrive active validators
       const [activeEra, err] = await this.chaindata.getActiveEraIndex();
-      // console.log(activeEra);
       const [blockHash, err2] = await this.chaindata.findEraBlockHash(activeEra);
-      // console.log(blockHash);
       const validators = await this.chaindata.getValidatorsByEraBlockHash(blockHash);
       const activeStash = validators.toHuman();
 
@@ -112,25 +111,16 @@ module.exports = class OnekvWrapper {
       return list;
     }
 
-    // console.log(list[0]);
-
     let amounts = list.map((item) => {
       return parseInt(item.amount, 10);
     })
     
     const to = moment.unix(list[0].block_timestamp);
     const from = moment.unix(list[list.length-1].block_timestamp);
-    // console.log('----------------')
-    // console.log(`Validator：${stash}`);
-    // console.log(`執行(天) ：${to.diff(from, 'days')}`);
-    // console.log(`收益(KSM)：${amounts.reduce((a, c) => {
-    //   return a + c
-    // })/1000000000000}`);
-    // console.log(`tx count: ${list.length}`)
 
     let totalReward = amounts.reduce((a, c) => {
       return a + c
-    })/1000000000000;
+    })/KUSAMA_DECIMAL;
 
     list = {
       stash,
